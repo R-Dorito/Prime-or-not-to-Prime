06f30e3666c9884fc00998c663348c0a7d39a81d
RITA UY <rita.uy56@gmail.com>
2021-01-27 13:29:39 +1100

    Is it prime?

--- Prime or not to Prime/Prime_or_not_to_Prime.py
+++ Prime or not to Prime/Prime_or_not_to_Prime.py
@@ -1,1 +1,17 @@
+import random
+
+game = False
+over = True
+
+while game is not over:
+    value = int(random.random() * 100)
+    print(value,"is this a prime?")
+    inp = input()
+    for n in range (1, int(value/2)):
+        if value % n == 0:
+             isPrime = False
+             print("no")
+        else:
+             print("yes")
+
 
